b7313a69dd45a7c005b33b88bfffee252337ad9e
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-18 17:56:58 +0300

    5.1.2. Создать итератор четные числа [#408058]

---
+++ src/main/java/ru/job4j/it/EvenIterator.java
@@ -0,0 +1,47 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+public class EvenIterator implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+
+    public EvenIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return findEvenNumberIndex() != -1;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        point++;
+        return data[findEvenNumberIndex()];
+    }
+
+    public int findEvenNumberIndex() {
+        return IntStream.range(point, data.length)
+                .filter(i -> data[i] % 2 == 0)
+                .findFirst()
+                .orElse(-1);
+    }
+
+    @Override
+    public void remove() {
+       throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void forEachRemaining(Consumer<? super Integer> action) {
+        throw new UnsupportedOperationException();
+    }
+}
