17311ee7df0114e1b32307223749767d1dd740c2
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-27 15:26:21 +0300

    3. Удалить head в односвязном списке. [#408048]

---
+++ src/test/java/ru/job4j/it/simplearray/ForwardLinkedTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.it.simplearray;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ForwardLinkedTest {
+
+    ForwardLinked linked;
+
+    @Before
+    public void setup() {
+        linked = new ForwardLinked();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        linked.add(4);
+        linked.deleteFirst();
+        linked.deleteFirst();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+    }
+}
