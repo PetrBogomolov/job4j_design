54e0823088b6ea1bee81a8fc49e8481f4855f4cb
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-08 18:56:35 +0300

    0.1. FileOutputStream. [#408099]

---
+++ src/main/java/ru/job4j/it/io/ResultFile.java
@@ -0,0 +1,25 @@
+package ru.job4j.it.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
+            out.write(convert(4).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String convert(int size) {
+        int[][] input = Matrix.multiple(size);
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                string.append(input[i][j]).append(" ");
+            }
+            string.append("\n");
+        }
+        return string.toString();
+    }
+}
