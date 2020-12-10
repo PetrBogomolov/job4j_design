ae6d2ebc948d8b9d220ca86ffa37b7101cf4185d
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-10 12:54:02 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#408045]

--- src/main/java/ru/job4j/it/mail/UniqueAccount.java
+++ src/main/java/ru/job4j/it/mail/UniqueAccount.java
@@ -7,91 +7,41 @@ import java.util.*;
  */
 public class UniqueAccount {
     /**
-     * accounts хранит связку данных: пользователь = множество email
-     */
-    private Map<User, Set<String>> accounts = new HashMap<>();
-
-    /**
-     * Метод добавления пользователя в мапу
-     *
-     * @param user Объект класса User - пользователь
-     */
-    public void addUser(User user) {
-        accounts.putIfAbsent(user, new HashSet<String>());
-    }
-
-    /**
-     * Метод привязывания email к пользователю
-     *
-     * @param name имя пользователя
-     * @param string адрес эл. почты
-     */
-    public void addEmail(String name, String string) {
-        User user = findByName(name).get();
-        Set<String> email = accounts.get(user);
-        accounts.merge(user,email, (a, b) -> added(b, string));
-    }
-
-    /**
-     * Метод добавляет элемент в множество.
-     * Используется для передачи в фукнцию в лямбда выражении при вызове метода
-     * Map.merge(K key, V value, BiFunction<? super V,? super V,? extends V> remappingFunction)
-     * т.к метод Set.add() нельзя использовать на прямую, т.к он возвращает boolean;
-     *
-     * @param set множество куда необходимо произвести добавление
-     * @param str элемент, который добавляем
-     * @return множество с добавленным элементом
-     */
-    private Set<String> added(Set<String> set, String str) {
-        set.add(str);
-        return set;
-    }
-
-    /**
-     * Метод ищет пользователя по его имени
-     *
-     * @param name имя пользователя
-     * @return объект класса User в Optional обертке
-     */
-    public Optional<User> findByName(String name) {
-        return accounts.keySet().stream()
-                .filter(e -> e.getName().equals(name))
-                .findFirst();
-    }
-
-    /**
-     * Метод находит множество email у пользователя
-     *
-     * @param user пользователь, чьи email находит метод
-     * @return множество email
-     */
-    public Set<String> findEmail(User user) {
-        return accounts.get(user);
-    }
-
-    /**
      * Метод находит общие email у пользователей.
      * Если он нашел общие email у двух пользователей,
      * он считает этих пользователей идентичными и объединяет их,
-     * путем слияния множеств email второго и первого пользователя,
+     * путем слияния множеств email-ов второго и первого пользователя,
      * ключом остается первый пользователь.
      *
-     * Set перевел в List для более быстрого доступа к ключам через индекс.
-     *
+     * @return новую мапу, в которой пользователи с одинаковыми значения были объединены
      */
-    public void mergeAccount() {
-        Set<User> key = accounts.keySet();
-        List<User> keys = new ArrayList<>(key);
-        for (int i = 0; i < keys.size(); i++) {
-            Set<String> userValue = accounts.get(keys.get(i));
-            Set<String> nextUserValue = accounts.get(keys.get(i + 1));
-            for (String str : nextUserValue) {
-                if (userValue.contains(str)) {
-                    userValue.addAll(nextUserValue);
-                    nextUserValue.removeAll(nextUserValue);
-                    return;
+    public Map<User, Set<String>> mergeAccount(Map<User, Set<String>> accounts) {
+        Map<User, Set<String>> result = new HashMap<>();
+        Map<String, User> emailToUser = new HashMap<>();
+        Set<Map.Entry<User, Set<String>>> setAccounts = accounts.entrySet();
+        User user = null;
+        for (Map.Entry<User, Set<String>> elementOfAccounts : setAccounts) {
+            for (String email : elementOfAccounts.getValue()) {
+                user = emailToUser.get(email);
+                if (user != null) {
+                    break;
                 }
             }
+            if (user == null) {
+                result.put(elementOfAccounts.getKey(), new HashSet<>(elementOfAccounts.getValue()));
+                for (String email : elementOfAccounts.getValue()) {
+                    emailToUser.put(email, elementOfAccounts.getKey());
+                }
+            }
+            if (user != null) {
+                Set<String> userEmails = result.get(user);
+                for (String email : elementOfAccounts.getValue()) {
+                    emailToUser.putIfAbsent(email, user);
+                    userEmails.add(email);
+                }
+                result.put(user, userEmails);
+            }
         }
+        return result;
     }
 }
