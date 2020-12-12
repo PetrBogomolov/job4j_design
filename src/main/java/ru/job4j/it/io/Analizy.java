7b7c8cf0389e05d931f5f77a085f6dcb7cde893c
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-12 11:25:55 +0300

    2. Анализ доступности сервера. [#408106]

--- src/main/java/ru/job4j/it/io/Analizy.java
+++ src/main/java/ru/job4j/it/io/Analizy.java
@@ -3,25 +3,27 @@ package ru.job4j.it.io;
 import java.io.*;
 
 public class Analizy {
+    private static final String REGEX = ";";
+    private static final String STATUS400 = "400";
+    private static final String STATUS500 = "500";
+
     public void unavailable(String sourse, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(sourse));
             PrintWriter write = new PrintWriter(new FileOutputStream(target))) {
-            StringBuilder newLine = new StringBuilder();
             String cursor = null;
             while (read.ready()) {
                 String currentLine = read.readLine();
-                if (cursor == null && (currentLine.contains("400") || currentLine.contains("500"))) {
+                if (cursor == null && (currentLine.contains(STATUS400) || currentLine.contains(STATUS500))) {
                     String[] lines = currentLine.split(" ");
-                    newLine.append(lines[1]).append(";");
+                    write.print(lines[1] + REGEX);
                     cursor = currentLine;
                 }
-                if (cursor != null && (!currentLine.contains("400") && !currentLine.contains("500"))) {
+                if (cursor != null && (!currentLine.contains(STATUS400) && !currentLine.contains(STATUS500))) {
                     String[] lines = currentLine.split(" ");
-                    newLine.append(lines[1]).append(";").append(System.lineSeparator());
+                    write.println(lines[1] + REGEX);
                     cursor = null;
                 }
             }
-            write.println(newLine.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
