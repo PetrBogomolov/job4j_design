b5de1291bf073aeb6c0618293938a886059a2958
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-12 13:59:37 +0300

    3.0. Тестирование IO [#408097]

--- src/test/java/ru/job4j/it/io/AnalizyTest.java
+++ src/test/java/ru/job4j/it/io/AnalizyTest.java
@@ -1,33 +1,30 @@
 package ru.job4j.it.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
-    public List<String> read(String file) {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader read = new BufferedReader(new FileReader(file))) {
-            lines = read.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return lines;
-    }
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void unavailable() {
+    public void unavailable() throws IOException {
         Analizy analizy = new Analizy();
-        String pathSourse = "server.log";
-        String pathTager = "unavailable.csv";
-        analizy.unavailable(pathSourse, pathTager);
-        List<String> result = read(pathTager);
+        String sourse = "server.log";
+        File target = folder.newFile("unavailable.csv");
+        analizy.unavailable(sourse, target.getAbsolutePath());
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(target.getAbsoluteFile()))) {
+            in.lines().forEach(result::add);
+        }
         Iterator<String> it = result.listIterator();
         assertThat(it.next(), is("10:58:01;10:59:01;"));
         assertThat(it.next(), is("11:01:02;11:02:02;"));
