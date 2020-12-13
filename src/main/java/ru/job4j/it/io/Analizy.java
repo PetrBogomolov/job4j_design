2b39d12d5dc769f83ddfa92a9f8dcea23e3a6080
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-13 12:43:38 +0300

    2. Анализ доступности сервера. [#408106]

--- src/main/java/ru/job4j/it/io/Analizy.java
+++ src/main/java/ru/job4j/it/io/Analizy.java
@@ -1,6 +1,8 @@
 package ru.job4j.it.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
     private static final String REGEX = ";";
@@ -8,23 +10,29 @@ public class Analizy {
     private static final String STATUS500 = "500";
 
     public void unavailable(String sourse, String target) {
-        try (BufferedReader read = new BufferedReader(new FileReader(sourse));
-            PrintWriter write = new PrintWriter(new FileOutputStream(target))) {
+        try (BufferedReader read = new BufferedReader(new FileReader(sourse))) {
+            List<String> result = new ArrayList<>();
             String cursor = null;
+            String storage = null;
             while (read.ready()) {
                 String currentLine = read.readLine();
                 if (cursor == null && (currentLine.contains(STATUS400) || currentLine.contains(STATUS500))) {
                     String[] lines = currentLine.split(" ");
-                    write.print(lines[1] + REGEX);
+                    storage = lines[1] + REGEX;
                     cursor = currentLine;
                 }
                 if (cursor != null && (!currentLine.contains(STATUS400) && !currentLine.contains(STATUS500))) {
                     String[] lines = currentLine.split(" ");
-                    write.println(lines[1] + REGEX);
+                    result.add(storage + lines[1] + REGEX);
                     cursor = null;
                 }
             }
-        } catch (Exception e) {
+            PrintWriter write = new PrintWriter(new FileOutputStream(target));
+            for (String element : result) {
+                write.println(element);
+            }
+            write.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
