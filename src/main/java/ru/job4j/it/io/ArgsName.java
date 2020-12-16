7127a4dc635377e961fced593a9c741e909eeaca
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-16 14:20:32 +0300

    5.1. Именованные аргументы [#408103]

---
+++ src/main/java/ru/job4j/it/io/ArgsName.java
@@ -0,0 +1,32 @@
+package ru.job4j.it.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Parameters not specified");
+        }
+        for (String parametr : args) {
+            if (!parametr.contains("=")) {
+                break;
+            } else {
+                String[] lines = parametr.split("=");
+                values.put(lines[0].replaceFirst("-", ""), lines[1]);
+            }
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName name = new ArgsName();
+        name.parse(args);
+        return name;
+    }
+}
