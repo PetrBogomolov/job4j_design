519c13fb15b0e5b062edf2deb76bb9e31a9d7214
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-16 19:39:39 +0300

    5.2. Архивировать проект [#408107]

---
+++ src/main/java/ru/job4j/it/io/archive/ArgZip.java
@@ -0,0 +1,43 @@
+package ru.job4j.it.io.archive;
+
+public class ArgZip {
+    private final String[] args;
+    private static final String DIRECTORY = "c:";
+    private static final String FILE = "*";
+    private static final String TARGET = ".zip";
+
+    public ArgZip(String[] args) {
+        this.args = args;
+    }
+
+    public void valid() {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Not all data entered");
+        }
+    }
+
+    private String chooseElement(String str) {
+        valid();
+        for (String element : args) {
+            if (element.contains(str)) {
+                return element;
+            }
+        }
+        throw new NullPointerException();
+    }
+
+    public String directory() {
+       return chooseElement(DIRECTORY);
+    }
+
+    public String exclude() {
+        return chooseElement(FILE).replace(FILE, "");
+    }
+
+    public String output() {
+       return chooseElement(TARGET);
+    }
+}
---
+++ src/main/java/ru/job4j/it/io/archive/Zip.java
@@ -0,0 +1,44 @@
+package ru.job4j.it.io.archive;
+
+import ru.job4j.it.io.search.Search;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<Path> sourse, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toString())))) {
+            for (Path element : sourse) {
+                zip.putNextEntry(new ZipEntry(element.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(sourse.toString()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void packSingleFile(Path sourse, Path target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(String.valueOf(target))))) {
+            zip.putNextEntry(new ZipEntry(sourse.toString()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(sourse.toString()))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgZip func = new ArgZip(args);
+        Path sourse = new File(func.directory()).toPath();
+        Path target = new File(func.output()).toPath();
+        String ext = func.exclude();
+        new Zip().packFiles(Search.search(sourse, ext), target);
+    }
+}
