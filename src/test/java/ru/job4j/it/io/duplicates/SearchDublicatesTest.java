67a1a645674e07d987dcffb3a74573e6e71975cc
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-17 12:35:45 +0300

    4.2. Поиск дубликатов [#408104]

--- src/test/java/ru/job4j/it/io/duplicates/SearchDublicatesTest.java
+++ src/test/java/ru/job4j/it/io/duplicates/SearchDublicatesTest.java
@@ -1,25 +1,36 @@
 package ru.job4j.it.io.duplicates;
 
-
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
+import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SearchDublicatesTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    private File firstDublicate;
+
+    @Before
+    public void setup() throws IOException {
+        folder.newFile("file1.txt");
+        folder.newFile("file2.txt");
+        folder.newFile("file3.txt");
+        folder.newFolder("folder");
+        firstDublicate = folder.newFile("folder//file1.txt");
+        firstDublicate = folder.newFile("folder//file2.txt");
+    }
+
     @Test
     public void whenDirectoryContainEqualsPath() throws IOException {
-        Path path = new File("C://Train").toPath();
-        List<String> result = SearchDublicate.searchDublicate(path);
-        Iterator<String> it = result.listIterator();
-        assertThat(it.next(), is("name file : file1.txt, size file : 0\r\n"));
-        assertThat(it.next(), is("name file : file2.txt, size file : 0\r\n"));
+      List<String> expect = List.of("name file : file1.txt, size file : 0" + System.lineSeparator(),
+               "name file : file2.txt, size file : 0" + System.lineSeparator());
+      assertThat(SearchDublicate.searchDublicate(folder.getRoot().toPath()), is(expect));
     }
 }
