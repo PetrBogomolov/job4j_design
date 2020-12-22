5d5d1b976d4b03bc28b8bfed736d1047ec2ff4f3
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-12-22 10:57:53 +0300

    1. Бот [#408122]

--- src/main/java/ru/job4j/it/socket/EchoServer.java
+++ src/main/java/ru/job4j/it/socket/EchoServer.java
@@ -14,17 +14,25 @@ public class EchoServer {
             while (runService) {
                 Socket socket = server.accept();
                 try (OutputStream write = socket.getOutputStream();
-                BufferedReader read = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()))) {
+                     BufferedReader read = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String answer = "";
                     while (!(str = read.readLine()).isEmpty()) {
-                        if (str.endsWith("Bye")) {
-                            server.close();
+                        if (str.contains("Exit")) {
                             runService = false;
+                            server.close();
+                            break;
+                        } else if (str.contains("Hello")) {
+                            answer = "Hello";
+                            break;
+                        } else {
+                            answer = "What";
+                            System.out.println(str);
                         }
-                        System.out.println(str);
                     }
-                    write.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    write.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    write.write(answer.getBytes());
                 }
             }
         }
