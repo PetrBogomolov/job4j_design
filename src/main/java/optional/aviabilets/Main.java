00957aebfb220a85262cab7fd7b5da195b3f43c4
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-01-09 08:23:26 +0300

    checkStyle

--- src/main/java/optional/aviabilets/Main.java
+++ src/main/java/optional/aviabilets/Main.java
@@ -3,7 +3,7 @@ package optional.aviabilets;
 import java.io.File;
 import java.nio.file.Path;
 
-public class Run {
+public class Main {
     public static void main(String[] args) {
         FilterDublicateFlights filter = new FilterDublicateFlights();
         Path source = new File("flights.txt").toPath();
