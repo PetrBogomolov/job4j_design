07e6f4f468a70c0d50d11a580e38c8331f71ddf3
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-01-28 14:41:30 +0300

    SQL Select by company [#408040]

--- src/main/java/ru/job4j/sql/jdbc/exam.sql
+++ src/main/java/ru/job4j/sql/jdbc/exam.sql
@@ -1,12 +1,8 @@
--- Имена всех работников, которые не состоят в компании с id = 5
-SELECT name, company_id
-FROM person
-WHERE company_id != 5;
-
--- Названия компании для каждого работника
+-- Имена всех работников, которые не состоят в компании с id = 5 + названия их компания
 SELECT p.name, c.name
 FROM person p
-         JOIN company c ON p.company_id = c.id;
+         JOIN company c ON p.company_id = c.id
+WHERE p.company_id != 5;
 
 -- Название компании с максимальным количеством человек + количество человек в этой компании
 SELECT c.name as название_компании, count(p.name) as число_сотрудников
