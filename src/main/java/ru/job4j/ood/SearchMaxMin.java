bfe9b6f04da946fd64ebc0ee07c30429fe6763a9
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-02-16 15:52:20 +0300

    1. Принципы Kiss, Dry и Yagni [#408125]

---
+++ src/main/java/ru/job4j/ood/SearchMaxMin.java
@@ -0,0 +1,25 @@
+package ru.job4j.ood;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SearchMaxMin {
+
+    public <T> T max(List<T> values, Comparator<T> comparator) {
+        return searchMax(values, comparator);
+    }
+
+    public <T> T min(List<T> values, Comparator<T> comparator) {
+        return searchMax(values, comparator.reversed());
+    }
+
+    private <T> T searchMax(List<T> values, Comparator<T> comparator) {
+        T max = values.get(0);
+        for (T value : values) {
+            if (comparator.compare(max, value) < 0) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
