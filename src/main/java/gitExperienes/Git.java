223ea30d92533c419d6c75f5c61cb3727c2df073
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-12-16 10:45:19 +0300

    first commit

---
+++ src/main/java/gitExperienes/Git.java
@@ -0,0 +1,9 @@
+package gitExperienes;
+
+public class Git {
+    private static String mainBranch = "main";
+
+    public static void main(String[] args) {
+        System.out.println(mainBranch);
+    }
+}
