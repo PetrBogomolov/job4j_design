7cf2f59a9f1a1a85ba86f83fc5bce1c775e86bfa
PetrBogomolov <BogomoloffPD@yandex.ru>
2023-10-22 17:05:21 +0300

    коммит добавляющий

--- src/main/java/gitExperienes/A.java
+++ src/main/java/gitExperienes/A.java
@@ -3,4 +3,5 @@ package gitExperienes;
 public class A {
     String srt = "Класс в основной в нем пройдет изменения в ветке миб 1, прошли изменения";
     String str2 = "new";
+    String str3 = "str2";
 }
